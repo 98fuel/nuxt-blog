830878271f09a495da2f1b8ba144e18285a72d38
Nov8nana <shuxhan@163.com>
2022-01-21 14:27:48 +0800

    Delete .nuxt directory

--- .nuxt/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Comments } from '../..\\components\\Comments.vue'
-export { default as Imgbig } from '../..\\components\\Imgbig.vue'
-export { default as Pager } from '../..\\components\\Pager.vue'
-
-export const LazyComments = import('../..\\components\\Comments.vue' /* webpackChunkName: "components_Comments'}" */).then(c => c.default || c)
-export const LazyImgbig = import('../..\\components\\Imgbig.vue' /* webpackChunkName: "components_Imgbig'}" */).then(c => c.default || c)
-export const LazyPager = import('../..\\components\\Pager.vue' /* webpackChunkName: "components_Pager'}" */).then(c => c.default || c)
